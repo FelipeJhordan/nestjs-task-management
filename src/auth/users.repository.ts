f3daf50711f9a70ce6b48aa59778fd6d08e087ab
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-12 11:58:41 -0300

    feat: implement signUp
    update auth.controller with correct handler (POST)
    update service business logic
    update repository with repository logic/error mapping
    update user entity for column username be unique

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,28 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
+  async createUser({
+    username,
+    password,
+  }: AuthCredentialsDto): Promise<void> {
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      }
+
+      throw new InternalServerErrorException();
+    }
+  }
   //
 }
