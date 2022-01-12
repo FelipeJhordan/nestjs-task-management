0e293436b6390d754f5b479e528d43c34eeb03a8
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-12 13:30:21 -0300

    feat: create method to signIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './auth-credentials.dto';
 import { UsersRepository } from './users.repository';
-
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(AuthCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(AuthCredentialsDto);
+  }
+
+  async signIn({
+    username,
+    password,
+  }: AuthCredentialsDto): Promise<{ accessToken: string }> {
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user && (await compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
+    }
+    throw new UnauthorizedException('Please check your login credentials');
   }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,6 +5,7 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -12,7 +13,10 @@ export class UsersRepository extends Repository<User> {
     username,
     password,
   }: AuthCredentialsDto): Promise<void> {
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
